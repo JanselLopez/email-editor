03521fa2b9f5e0ada51e382f441993c7df57fc10
JanselLopez <21jansel@gmail.com>
2026-02-27 11:56:39 -0500

    chore: deploy gp

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/email-editor/', // Para GitHub Pages: https://<usuario>.github.io/email-templates/
 })
